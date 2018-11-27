8f3f15cc36c32dae24ed37acef91ca8b869628db
Issa Rice <riceissa@gmail.com>
2018-11-27 15:12:51 -0800

    more

--- HelloAnalytics.py
+++ HelloAnalytics.py
@@ -34,6 +34,7 @@ def main():
             for row in reader:
                 # print(row['project_title'])
                 data.append((row['project_title'],
+                             row['url'],
                              get_pageviews_for_project(analytics, row['view_id'], 2017, 10)))
         with open("analytics_data.pickle", "wb") as f:
             pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
@@ -43,28 +44,73 @@ def main():
 
 
 def print_table(data):
-    all_months = sorted(set.union(*[set(pageviews_dict.keys()) for _, pageviews_dict in data]),
-                        key=lambda x: datetime.datetime.strptime(x, "%B %Y"))
+    all_months = sorted(set.union(*[set(pageviews_dict.keys()) for _, _, pageviews_dict in data]),
+                        key=lambda x: datetime.datetime.strptime(x, "%B %Y"),
+                        reverse=True)
+    print('''<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+      <link rel="stylesheet" href="./tablesorter.css">
+          <script src="./jquery.min.js"></script>
+          <script src="./jquery.tablesorter.js"></script>
+        <style type="text/css">
+        body { font-size: 12px; }
+        table {
+            background-color: #f9f9f9;
+            border-collapse: collapse;
+        }
+        table th {
+            background-color: #f2f2f2;
+            border: 1px solid #aaaaaa;
+            padding: 5px 10px;
+        }
+        table td {
+            border: 1px solid #aaaaaa;
+            padding: 5px 10px;
+        }
+        </style>
+
+        </head>
+        <body>
+      ''')
 
     print("<table>")
+    print("<thead>")
     print("  <tr>")
     print("  <th>Project title</th>")
+    print("  <th>Total</th>")
     for month in all_months:
         print("  <th>")
         print(month)
         print("  </th>")
     print("  </tr>")
-    for project_title, pageviews_dict in data:
+    print("</thead>")
+    print("<tbody>")
+    for project_title, url, pageviews_dict in data:
         print("<tr>")
-        print('''<td>%s</td>''' % project_title)
+        print('''<td><a href="%s">%s</a></td>''' % (url, project_title))
+        print('''<td style="text-align: right;">%s</td>''' % sum(pageviews_dict.values()))
         for month in all_months:
             if month in pageviews_dict:
-                print('''<td>%s</td>''' % pageviews_dict[month])
+                print('''<td style="text-align: right;">%s</td>''' % pageviews_dict[month])
             else:
-                print("<td>n.a.</td>")
+                print('''<td style="text-align: right;">n.a.</td>''')
         print("</tr>")
 
+    print("</tbody>")
     print("</table>")
+    print('''
+        <script>
+        $(function(){
+            $("table").tablesorter();
+          });
+    </script>
+    </body>
+    </html>
+    ''')
 
 
 
