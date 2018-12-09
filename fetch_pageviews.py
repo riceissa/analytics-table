70142c22e81613b2877b3aac95fa5c392836de27
Issa Rice <riceissa@gmail.com>
2018-12-09 15:47:28 -0800

    clean up

--- fetch_pageviews.py
+++ fetch_pageviews.py
@@ -40,7 +40,7 @@ def main():
             # very beginning
             last_date = start_date
 
-        pg = get_pageviews_for_project(analytics, view_id, last_date)
+        pg = pageviews_for_project(analytics, view_id, last_date)
         print(get_pageviews(pg[1]))
         # pdb.set_trace()
 
@@ -130,48 +130,17 @@ def extracted_pageviews(response):
     return result
 
 
-def get_pageviews_for_project(analytics, view_id, start_date):
-    # result = {}
-
-    # date_ranges = []
-    # for year in range(start_date.year, 2018+1):
-    #     if year == start_date.year:
-    #         months = range(start_date.month, 12+1)
-    #     else:
-    #         months = range(1, 12+1)
-    #     for month in months:
-    #         year_month_name = datetime.date(year, month, 1).strftime("%B %Y")
-    #         first_day = datetime.date(year, month, 1).strftime("%Y-%m-%d")
-    #         last_day = last_day_of_month(year, month).strftime("%Y-%m-%d")
-    #         date_ranges.append((year_month_name, first_day, last_day))
-
-    # for year_month_name, start_date, end_date in date_ranges:
-    #     response = get_report(analytics, view_id, start_date, end_date)
-    #     pageviews = get_pageviews(response)
-    #     assert len(pageviews) <= 1, (pageviews, year, month)
-    #     if len(pageviews) == 1:
-    #         result[year_month_name] = int(pageviews[0])
-
-    # return result
-
+def pageviews_for_project(analytics, view_id, start_date):
     page_token = None
-
     result = []
-
     while True:
         response = get_report(analytics, view_id, start_date.strftime("%Y-%m-%d"),
                               "2018-12-09", page_token)
-
-        result.append(response)
-
+        result.extend(extracted_pageviews(response))
         if "nextPageToken" in response["reports"][0]:
             page_token = response["reports"][0]["nextPageToken"]
         else:
             break
-
-    # pdb.set_trace()
-    # pageviews = get_pageviews(response)
-
     return result
 
 
