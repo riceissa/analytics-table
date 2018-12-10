6fbaeefa22c27761be0f8d371f82b0372da06448
Issa Rice <riceissa@gmail.com>
2018-12-09 18:43:56 -0800

    get table to print

--- print_table.py
+++ print_table.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python3
 
+# import pdb
+
+import mysql.connector
 import matplotlib.pyplot as plt
 import base64
 import io
+import sys
+
+import login
+
+def main():
+    cnx = mysql.connector.connect(user=login.USER, database=login.DATABASE,
+                                  password=login.PASSWORD)
+    cursor = cnx.cursor()
+    cursor.execute("""
+           select
+               project_title,
+               sum(pageviews),
+               year(pageviews_date),
+               month(pageviews_date)
+           from pageviews
+           group by
+               project_title,
+               year(pageviews_date),
+               month(pageviews_date)""")
+    pageviews_data = cursor.fetchall()
+
+    cursor.execute("""select project_title, url from projects""")
+    projects = cursor.fetchall()
+    # pdb.set_trace()
+    print_table(projects, pageviews_data)
 
 def plot_data(data):
     for project_title, _, pageviews_dict in data:
@@ -20,44 +48,42 @@ def plot_data(data):
 
 
 
-def print_table(data):
-    all_months = sorted(set.union(*[set(pageviews_dict.keys()) for _, _, pageviews_dict in data]),
-                        key=lambda x: datetime.datetime.strptime(x, "%B %Y"),
+def print_table(projects, pageviews_data):
+    all_months = sorted(set((year, month) for _, _, year, month in pageviews_data),
                         reverse=True)
+
     print('''<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-
-      <link rel="stylesheet" href="./tablesorter.css">
-          <script src="./jquery.min.js"></script>
-          <script src="./jquery.tablesorter.js"></script>
-        <style type="text/css">
+      <link rel="stylesheet" href="/tablesorter.css">
+      <script src="/jquery.min.js"></script>
+      <script src="/jquery.tablesorter.js"></script>
+      <style type="text/css">
         body { font-size: 12px; }
         table {
-            background-color: #f9f9f9;
-            border-collapse: collapse;
+          background-color: #f9f9f9;
+          border-collapse: collapse;
         }
         table th {
-            background-color: #f2f2f2;
-            border: 1px solid #aaaaaa;
-            padding: 5px 10px;
+          background-color: #f2f2f2;
+          border: 1px solid #aaaaaa;
+          padding: 5px 10px;
         }
         table td {
-            border: 1px solid #aaaaaa;
-            padding: 5px 10px;
+          border: 1px solid #aaaaaa;
+          padding: 5px 10px;
         }
-        </style>
-
-        </head>
-        <body>
-      ''')
+      </style>
+    </head>
+    <body>
+    ''')
 
     print("<table>")
-    print('''
-        <img src="data:image/png;base64, %s" />
-    ''' % plot_data(data))
+    # print('''
+    #     <img src="data:image/png;base64, %s" />
+    # ''' % plot_data(data))
     print("<thead>")
     print("  <tr>")
     print("  <th>Project title</th>")
@@ -69,13 +95,15 @@ def print_table(data):
     print("  </tr>")
     print("</thead>")
     print("<tbody>")
-    for project_title, url, pageviews_dict in data:
+    for project_title, url in projects:
+        data = {(year, month): views for title, views, year, month in pageviews_data
+                if title == project_title}
         print("<tr>")
         print('''<td><a href="%s">%s</a></td>''' % (url, project_title))
-        print('''<td style="text-align: right;">%s</td>''' % sum(pageviews_dict.values()))
+        print('''<td style="text-align: right;">%s</td>''' % sum(data.values()))
         for month in all_months:
-            if month in pageviews_dict:
-                print('''<td style="text-align: right;">%s</td>''' % pageviews_dict[month])
+            if month in data:
+                print('''<td style="text-align: right;">%s</td>''' % data[month])
             else:
                 print('''<td style="text-align: right;">n.a.</td>''')
         print("</tr>")
@@ -92,4 +120,5 @@ def print_table(data):
     </html>
     ''')
 
-
+if __name__ == "__main__":
+    main()
