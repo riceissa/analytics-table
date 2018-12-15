e830a1c8abdee39bf7baaa81927391600cf68a72
Issa Rice <riceissa@gmail.com>
2018-12-15 13:57:12 -0800

    accept start_date and end_date

--- access-portal/index.php
+++ access-portal/index.php
@@ -1,5 +1,23 @@
 <?php
 
-$command = "PYTHONIOENCODING=utf-8 ../print_table.py ";
+if ($_REQUEST['start_date'] ?? '') {
+  $start_date = $_REQUEST['start_date'];
+  $start_date = preg_replace('/[^0-9-]/', '', $start_date);
+} else {
+  $start_date = "";
+}
+
+if ($_REQUEST['end_date'] ?? '') {
+  $end_date = $_REQUEST['end_date'];
+  $end_date = preg_replace('/[^0-9-]/', '', $start_date);
+} else {
+  $end_date = "";
+}
+
+if (($start_date != "") || ($end_date != "")) {
+  $command = "PYTHONIOENCODING=utf-8 ../print_table.py " . escapeshellarg($start_date) . " " . escapeshellarg($end_date);
+} else {
+  $command = "PYTHONIOENCODING=utf-8 ../print_table.py ";
+}
 $output = shell_exec($command);
 echo $output;
