590f4e5e0d3463fff43f5163ef342606810940ea
Vipul Naik <vipulnaik1@gmail.com>
2019-06-09 00:04:41 +0000

    Add Diet Watch to projects list

--- sql/projects.sql
+++ sql/projects.sql
@@ -44,4 +44,5 @@ insert into projects(project_title, url, view_id, start_date) values
     ,('Content Creation Wiki', 'https://contentcreation.issarice.com/', '156584158', '2017-07-01')
     ,('Issa Rice', 'https://issarice.com/', '104557782', '2015-05-01')
     ,('Computing data project', 'https://computingdata.vipulnaik.com/', '194324723', '2019-04-28')
+    ,('Diet Watch','https://dietwatch.vipulnaik.com/','UA-141732375-1', '2019-06-08')
 ;
